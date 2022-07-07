a5c3bb8b6fe9e76afbe021efa2abe1dbeb93c89c
wdavidcch <david.13.2121@gmail.com>
2022-07-07 14:59:42 -0400

    Functions to edit and remove tasks

--- src/index.js
+++ src/index.js
@@ -26,38 +26,63 @@ const addTaskStorage = (task) => {
   localStorage.setItem('tasks', JSON.stringify(tasksArray));
 };
 
-// const removeTaskStorage = (bookdescription) => {
-//     let tasksArray = getBooks();
-//     tasksArray = tasksArray.filter((book) => book.description !== bookdescription);
-//     localStorage.setItem('tasks', JSON.stringify(tasksArray));
-//   }
-
 const addTaskScreen = (task) => {
   const tasksList = document.getElementById('list');
   const taskLine = document.createElement('ul');
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
   const description = document.createElement('h3');
   const options = document.createElement('span');
-  // const removeButton = document.createElement("span");
-  // removeButton.classList.add("delete");
+  const removeButton = document.createElement('span');
+  removeButton.classList.add('delete');
+  options.classList.add('options');
 
-  description.innerHTML = `<input type='checkbox'>&nbsp&nbsp${task.description}`;
+  description.innerHTML = task.description;
   options.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
-  // removeButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  removeButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
 
   tasksList.appendChild(taskLine);
-  taskLine.append(description, options);
+  taskLine.append(checkBox, description, options, removeButton);
+
+  taskLine.addEventListener('click', () => {
+    options.classList.add('active');
+    removeButton.classList.add('active');
+    taskLine.classList.add('active');
+
+    const editField = document.createElement('input');
+    editField.classList.add('edit-input');
+    editField.value = description.textContent;
+    taskLine.replaceChild(editField, description);
+
+    editField.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        // tasksArray = JSON.parse(localStorage.getItem('tasks'));
+        tasksArray[task.index].description = editField.value;
+        localStorage.setItem('tasks', JSON.stringify(tasksArray));
+
+        taskLine.replaceChild(description, editField);
+        description.innerHTML = task.description;
+        taskLine.classList.remove('active');
+        options.classList.remove('active');
+        removeButton.classList.remove('active');
+      }
+    });
+
+    removeButton.addEventListener('click', () => {
+      removeButton.parentElement.remove();
+      tasksArray.splice(task.index, 1);
+      tasksArray.forEach((task, index) => {
+        task.index = index;
+      });
+      localStorage.setItem('tasks', JSON.stringify(tasksArray));
+    });
+  });
 };
 
 const displayTasks = () => {
   tasksArray = getTasks();
   tasksArray.forEach((task) => addTaskScreen(task));
 };
-
-// static removeBook(eTarget) {
-//   if (eTarget.classList.contains('delete')) {
-//     eTarget.parentElement.remove();
-//   }
-// }
 
 enterNewTask.addEventListener('keypress', (e) => {
   if (e.keyCode === 13) {
